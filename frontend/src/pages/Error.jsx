dc55670e25c3f65ab3b804518c854932e6d82bf5
MrLad01 <ladayodeji@gmail.com>
2023-05-07 17:38:03 +0100

    update

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,7 +1,12 @@
+import { useRouteError } from "react-router-dom"
+
 const Error = () => {
+  const error = useRouteError()
+  console.log(error);
   return (
-    <div>
-      Error
+    <div className="error">
+      <h1>Uh oh! We've got a problem.</h1>
+      <p>{error.message || error.statusText}</p>
     </div>
   )
 }
