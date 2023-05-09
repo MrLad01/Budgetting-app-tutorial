1c5b23ed182143a48ac2b8320034f52844a6e1bd
MrLad01 <ladayodeji@gmail.com>
2023-05-09 07:45:21 +0100

    update

--- frontend/src/helpers.js
+++ frontend/src/helpers.js
@@ -1,4 +1,4 @@
-export const waait = () => new Promise(res => setTimeout(res, Math.random() * 2000 ));
+export const waait = () => new Promise(res => setTimeout(res, Math.random() * 800 ));
 
 const generateRandomColor = () => {
     const existingBudgetLength = fetchData("budgets")?.length ?? 0;
